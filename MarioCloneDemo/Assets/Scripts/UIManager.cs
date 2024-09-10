612034a4da297997764e0704ecb6fdb5aa0bf338
Elliott Kelly <elliottckelly@gmail.com>
2024-09-10 17:51:38 -0400

    Week3 Lab1 Completed

---
+++ MarioCloneDemo/Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    int score = 0;
+    public static UIManager instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "x" + score;
+    }
+}
